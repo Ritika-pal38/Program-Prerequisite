1ed252961bcbd49c9a359a983848972ec3ee739c
Ritika-pal38 <palritika294@gmail.com>
2025-01-08 11:32:19 +0530

    area of circle added

---
+++ Area_Of_Circle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+      class AreaOfCircle{
+	        public static void main(String[]args){
+			    
+			int radius=0;
+            double area=0.0;
+			int pi = 3.14;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter radius of circle:")'
+            area = 	pi*radius^2;
+            System.out.print("The area of circle is:"+area);
+            sc.close();
+            }
+     }			
